0117501ceaca4abc40a33dd10cce3c0170c69ed0
tunv30-fsoft <nguyenvantu131096@gmail.com>
2022-11-17 15:26:34 +0700

    commit fix bug

--- client/src/components/Todos.tsx
+++ client/src/components/Todos.tsx
@@ -46,6 +46,7 @@ export class Todos extends React.PureComponent<TodosProps, TodosState> {
 
   onTodoCreate = async (event: React.ChangeEvent<HTMLButtonElement>) => {
     try {
+      if(!this.state.newTodoName.trim()) return alert('Todo name is empty')
       const dueDate = this.calculateDueDate()
       const newTodo = await createTodo(this.props.auth.getIdToken(), {
         name: this.state.newTodoName,
