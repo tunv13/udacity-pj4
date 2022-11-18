fbb6a0a6bc89e3ec20e6a9d818fc9f577277d1c8
tunv30-fsoft <nguyenvantu131096@gmail.com>
2022-11-18 10:41:16 +0700

    Update todosAcess.ts

--- backend/src/dataLayer/todosAcess.ts
+++ backend/src/dataLayer/todosAcess.ts
@@ -1,18 +1,18 @@
 import * as AWS from 'aws-sdk'
-//import * as AWSXRay from 'aws-xray-sdk'
+import * as AWSXRay from 'aws-xray-sdk'
 import { DocumentClient } from 'aws-sdk/clients/dynamodb'
 import { createLogger } from '../utils/logger'
 import { TodoItem } from '../models/TodoItem'
 import { TodoUpdate } from '../models/TodoUpdate'
 
-//const XAWS = AWSXRay.captureAWS(AWS)
+const XAWS = AWSXRay.captureAWS(AWS)
 
 const logger = createLogger('TodosAccess')
 
 // TODO: Implement the dataLayer logic
 class TodoAccess {
- 
-  dynamoClient: DocumentClient = new AWS.DynamoDB.DocumentClient()
+
+  dynamoClient: DocumentClient | any = new XAWS.DynamoDB()
   
   todoTable = process.env.TODOS_TABLE
 
@@ -24,6 +24,7 @@ class TodoAccess {
         TableName: this.todoTable,
         Item: body
       }
+      
       await this.dynamoClient.put(params).promise()
       return body
     } catch (error) {
